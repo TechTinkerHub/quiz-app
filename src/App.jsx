1764279a90ac235cf6d9c7f5c0fd94e6fcf1abb0
AMEN <44getcha@gmail.com>
2024-04-13 13:32:04 +0300

    first

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import Quize from './componentes/Quize/Quize'
+
+const App = () => {
+  return (
+    <>
+      <Quize/>
+    </>
+  )
+}
+
+export default App
